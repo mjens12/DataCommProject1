4e5108d4a2b9a3b4303dc6fb8ae7d5cca71c7458
Isfar Baset <43085165+IsfarBaset19@users.noreply.github.com>
2019-10-13 14:26:55 -0400

    retr command

--- src/FTPServer.java
+++ src/FTPServer.java
@@ -1,65 +1,116 @@
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.InputStreamReader;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.net.*;
+import java.util.*;
 
-class FTPServer {
+class FTPServer
+{
 
-	public static void main(String argv[]) throws Exception {
-		String fromClient;
-		String clientCommand;
-		byte[] data;
-		int port = 0;
+	public static void main(String[] args) throws IOException
 
-		ServerSocket welcomeSocket = new ServerSocket(12000);
-		String frstln;
+	{
+        int port = 12000;
 
-		while (true) {
-			Socket connectionSocket = welcomeSocket.accept();
+        // Create socket server and wait for client to connect
+        ServerSocket welcomeSocket = new ServerSocket(port);
+        do{
+        Socket connectionSocket = welcomeSocket.accept();
+        System.out.println("\n\nUser Connected!\n\n");
 
-			DataOutputStream outToClient = new DataOutputStream(
-					connectionSocket.getOutputStream());
-			BufferedReader inFromClient =
-					new BufferedReader(new InputStreamReader(
-							connectionSocket.getInputStream()));
+       
+	//Creating threads
+	ClientHandler handler = new ClientHandler(connectionSocket);
+	handler.start();
+	}while (true);
+ }
+}
 
-			fromClient = inFromClient.readLine();
+class ClientHandler extends Thread{
 
-			StringTokenizer tokens = new StringTokenizer(fromClient);
-			frstln = tokens.nextToken();
-			port = Integer.parseInt(frstln);
-			clientCommand = tokens.nextToken();
+private DataOutputStream outToClient;
+private BufferedReader inFromClient;
 
-			if (clientCommand.equals("list:")) {
 
-				Socket dataSocket = new Socket(
-						connectionSocket.getInetAddress(), port);
-				DataOutputStream dataOutToClient =
-						new DataOutputStream(
-								dataSocket.getOutputStream());
+String fromClient;
+String clientCommand;
+byte[] data;
+int port = 12000;
+File directory = new File("/Users/isfar/Desktop/CIS457/Project_1");
+List<String> listOfFiles = new ArrayList<>();
+String firstln;
+private Socket connectionSocket;
 
-				// Max ..........................
 
-				dataSocket.close();
-				System.out.println("Data Socket closed");
-			}
+public ClientHandler(Socket socket)
+{
+	connectionSocket = socket;
 
-			if (clientCommand.equals("retr:")) {
-				// Isfar..............................
-				// ..............................
-			}
+	try {
+            outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+            inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+        } catch (IOException e) {
+           System.out.println("Error in connection");
+           System.out.println(e);
+        }
+}
 
-			if (clientCommand.equals("stor:")) {
-				// Sean ..............................
-				// ..............................
-			}
+public static void serverFiles(File directory, List<String> listOfFiles){
 
-			if (clientCommand.equals("quit")) {
-				// Sean ..............................
-				// ..............................
-			}
-		}
+        for(File file: directory.listFiles()){
+            if(file.isFile()){
+                listOfFiles.add(file.getName());
+            }
+        }
+    }
+
+	public void run(){
+
+		try{
+
+		fromClient = inFromClient.readLine();
+		StringTokenizer tokens = new StringTokenizer(fromClient);
+        firstln = tokens.nextToken();
+        port = Integer.parseInt(firstln);
+        clientCommand = tokens.nextToken();
+
+        
+        serverFiles(directory,listOfFiles);
+
+        if (clientCommand.equals("retr")) {
+
+        	Socket dataSocket = new Socket(connectionSocket.getInetAddress(), port);
+            System.out.println(connectionSocket.getInetAddress());
+            DataOutputStream dataOutToClient = new DataOutputStream(dataSocket.getOutputStream());
+
+        
+            String fileName = tokens.nextToken();
+        
+
+        if(!listOfFiles.contains(fileName)){
+        System.out.println("Error: File Not Found");
+        }
+
+        else{
+        	
+            File sendFile = new File(directory, fileName);
+            byte[] bytes = new byte[(int) sendFile.length()];
+            FileInputStream inputStream = new FileInputStream(sendFile);
+            inputStream.read(bytes);
+            inputStream.close();
+
+            dataOutToClient.write(bytes, 0, bytes.length);
+             
+           }
+         
+           dataSocket.close();
+           System.out.println("Data Socket closed");
+}
+
 	}
+
+	catch (IOException e) {
+            System.out.println("Error: Unable to disconnect");
+            System.out.println(e);
+        }
+}
+
 }
