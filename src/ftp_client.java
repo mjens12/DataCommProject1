4124c7f36f0479c48dfb5f5383aeec6bd453097e
Robert Collins <collirob@mail.gvsu.edu>
2019-10-14 19:54:40 -0400

    Changes

--- src/ftp_client.java
+++ src/ftp_client.java
@@ -18,21 +18,16 @@ public class ftp_client {
 		int port = 1200;
 		int port1 = port + 2;
 
-		BufferedReader inFromUser = new BufferedReader(
-				new InputStreamReader(System.in));
+		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
 		Socket controlSocket = new Socket("127.0.0.1", port);
 
 		System.out.println("You are connected to the server");
-		System.out.println(
-				"\nWhat would you like to do : \n retr: file.txt ||stor: file.txt  || quit\n");
+		System.out.println("\nWhat would you like to do : \nlist: || retr: file.txt || stor: file.txt  || quit\n");
 
 		while (isOpen && clientGo) {
 
-			DataOutputStream outToServer = new DataOutputStream(
-					controlSocket.getOutputStream());
-			DataInputStream inFromServer = new DataInputStream(
-					new BufferedInputStream(
-							controlSocket.getInputStream()));
+			DataOutputStream outToServer = new DataOutputStream(controlSocket.getOutputStream());
+			DataInputStream inFromServer = new DataInputStream(new BufferedInputStream(controlSocket.getInputStream()));
 			sentence = inFromUser.readLine();
 			StringTokenizer tokens = new StringTokenizer(sentence);
 
@@ -40,15 +35,12 @@ public class ftp_client {
 				// Max
 
 				port = port + 2;
-				outToServer.writeBytes(
-						port + " " + sentence + " " + '\n');
+				outToServer.writeBytes(port + " " + sentence + " " + '\n');
 
 				ServerSocket welcomeData = new ServerSocket(port);
 				Socket dataSocket = welcomeData.accept();
 
-				DataInputStream inData = new DataInputStream(
-						new BufferedInputStream(
-								dataSocket.getInputStream()));
+				DataInputStream inData = new DataInputStream(new BufferedInputStream(dataSocket.getInputStream()));
 				try {
 					modifiedSentence = inData.readUTF();
 					System.out.print(modifiedSentence);
@@ -59,85 +51,68 @@ public class ftp_client {
 				welcomeData.close();
 				dataSocket.close();
 				System.out.println(
-						"\nWhat would you like to do next: \n retr: file.txt ||stor: file.txt  || quit");
+						"\nWhat would you like to do next: \nlist: || retr: file.txt ||stor: file.txt || quit");
 			} else if (sentence.equals("retr:")) {
-
-				int bytesRead;
-				int current = 0;
-				int filesize = 10;
-				byte[] byteArray = new byte[10];
 				port = port + 2;
+				String fileName = tokens.nextToken();
+				outToServer.writeBytes(port + " " + sentence + " " + fileName + '\n');
 
-				outToServer.writeBytes(
-						port + " " + sentence + " " + '\n');
 				ServerSocket welcomeData = new ServerSocket(port);
 				Socket dataSocket = welcomeData.accept();
 
-				//Might not need to supply a filename, the incoming file should already have a name
-				String filename = "file.txt";
-				//We probably need to create a new file here to hold the incoming file
-				//Maybe pull the name from the file request message to the server?
-				File file = new File(filename);
-				FileInputStream fis = new FileInputStream(file);
+				DataInputStream inData = new DataInputStream(new BufferedInputStream(dataSocket.getInputStream()));
 
-				BufferedInputStream bis = new BufferedInputStream(
-						fis);
-				ByteArrayOutputStream byteos = new ByteArrayOutputStream();
+				int filesize = 6022386;
+				int bytesRead;
+				int current = 0;
+				byte[] mybytearray = new byte[filesize];
 
-				// Need to figure out how things work below
-				// I just changed the relevant output stuff to input so nothing will work yet
-
-				bytesRead = fis.read(byteArray, 0, byteArray.length);
-
+				FileOutputStream fos = new FileOutputStream("def");
+				BufferedOutputStream bos = new BufferedOutputStream(fos);
+				bytesRead = inData.read(mybytearray, 0, mybytearray.length);
 				current = bytesRead;
 
 				do {
-					byteos.write(byteArray);
-					current = fis.read(byteArray);
-				} while (current > -1);
+					bytesRead = inData.read(mybytearray, current, (mybytearray.length - current));
+					if (bytesRead >= 0)
+						current += bytesRead;
+				} while (bytesRead > -1);
 
-				bis.write(byteos.toByteArray());
-				bis.flush();
-				bis.close();
+				bos.write(mybytearray, 0, current);
+				bos.flush();
+				bos.close();
 
 				System.out.println("\nFile Retrieved");
 
-				bis.flush();
-				bis.close();
 				welcomeData.close();
 				dataSocket.close();
 				System.out.println(
-						"\nWhat would you like to do next: \n retr: file.txt ||stor: file.txt  || quit");
+						"\nWhat would you like to do next: \nlist: || retr: file.txt || stor: file.txt || quit");
 
 			}
 
 			else if (sentence.startsWith("stor: ")) {
 				// Rob
 				port = port + 2;
-				outToServer.writeBytes(
-						port + " " + sentence + " " + '\n');
+				outToServer.writeBytes(port + " " + sentence + " " + '\n');
 
 				ServerSocket welcomeData = new ServerSocket(port);
 				Socket dataSocket = welcomeData.accept();
 
-				DataInputStream inData = new DataInputStream(
-						new BufferedInputStream(
-								dataSocket.getInputStream()));
+				DataOutputStream outData = new DataOutputStream(new BufferedOutputStream(dataSocket.getOutputStream()));
 
 				String fileName = tokens.nextToken();
 				File clientFilesDirectory = new File(".");
-				String filePath = clientFilesDirectory + "/"
-						+ fileName;
+				String filePath = clientFilesDirectory + "/" + fileName;
 				File file = new File(filePath);
 
 				if (file.exists()) {
 					byte[] bytes = new byte[(int) file.length()];
-					FileInputStream inputStream = new FileInputStream(
-							file);
-					inputStream.read(bytes);
-					inputStream.close();
+					FileOutputStream outputStream = new FileOutputStream(file);
+					outputStream.write(bytes);
+					outputStream.close();
 
-					dataToServer.write(bytes, 0, bytes.length);
+					outData.write(bytes, 0, bytes.length);
 				}
 
 				else {
@@ -152,6 +127,8 @@ public class ftp_client {
 
 			else if (sentence.startsWith("quit")) {
 				// Rob
+				port = port + 2;
+				outToServer.writeBytes(port + " " + sentence + " " + "\n");
 				System.out.println("\nGoodbye");
 				System.exit(0);
 			}
